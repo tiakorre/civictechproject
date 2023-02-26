39f706d62bab863eed64eec82ac11da05f038422
bellad21992 <120699047+bellad21992@users.noreply.github.com>
2023-02-26 01:03:21 -0500

    Create closest_stores.py

---
+++ closest_stores.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+
+# Set up the Google Maps API request
+api_key = 'AIzaSyDYFAVWtbx70nJZUFsqUFuQE_RA7S-uQ1A'
+url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+
+# Get the user's location
+geolocator = Nominatim(user_agent='my_application')
+address = input('Enter your address or ZIP code: ')
+location = geolocator.geocode(address)
+user_lat, user_lon = location.latitude, location.longitude
+
+# Make the Places API request
+stores = []
+params = {
+    'key': api_key,
+    'location': f'{user_lat},{user_lon}',
+    'radius': 8046.72,  # 5 miles in meters
+    'type': 'grocery_or_supermarket'
+}
+response = requests.get(url, params=params)
+results = response.json()['results']
+for result in results:
+    lat, lon = result['geometry']['location']['lat'], result['geometry']['location']['lng']
+    store_location = (lat, lon)
+    distance = geodesic((user_lat, user_lon), store_location).miles
+    if distance <= 5:
+        place_id = result['place_id']
+        details_url = 'https://maps.googleapis.com/maps/api/place/details/json'
+        details_params = {
+            'key': api_key,
+            'place_id': place_id,
+            'fields': 'name,website'
+        }
+        details_response = requests.get(details_url, params=details_params)
+        details_result = details_response.json()['result']
+        if 'website' in details_result:
+            store_name = details_result['name']
+            store_url = details_result['website']
+            stores.append((store_name, distance, store_url))
+
+# Sort the list of stores by distance
+sorted_stores = sorted(stores, key=lambda x: x[1])
+
+# Print the name, distance, and URL for each store within 5 miles, sorted by distance
+for store in sorted_stores:
+    print(f'{store[0]} ({store[1]:.2f} miles away): {store[2]}')
